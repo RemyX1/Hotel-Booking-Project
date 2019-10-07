0ef323837645c6db29af81584e7e4f1d84bbbfd3
Remy <remington49@rocketmail.com>
2019-10-07 14:45:52 -0400

    Add

--- eureka-server/src/test/java/com/trilogyed/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/trilogyed/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.triologyed.eurekaserviceregistry;
+package com.trilogyed.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServiceRegistryApplicationTests {
+public class EurekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
---
+++ hotel-booking-service/src/main/java/com/company/trilogyed/hotelbookingservice/HotelBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.trilogyed.hotelbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingServiceApplication.class, args);
+	}
+
+}
