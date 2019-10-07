811797de099ed66c45ae91dc67f87cdc2dadf33f
Remy <remington49@rocketmail.com>
2019-10-07 17:35:27 -0400

    Beep Bop Boop beep Master branch merge me

---
+++ hotel-booking-service/src/main/java/com/company/trilogyed/hotelbookingservice/controller/HotelController.java
@@ -0,0 +1,40 @@
+package com.company.trilogyed.hotelbookingservice.controller;
+
+import com.company.trilogyed.hotelbookingservice.model.RoomViewModel;
+import com.company.trilogyed.hotelbookingservice.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class HotelController {
+
+
+
+    @Autowired
+    private Service service;
+
+    public HotelController(Service service) {
+        this.service = service;
+    }
+
+    //Pretend this looks good.
+    //Sorry Barbara.
+    @RequestMapping(value="/hotelRewards/{roomId}?rewardsMember=[true|false]&doubleBonusDay=[true|false]",
+            method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public RoomViewModel createRoomViewModel(@PathVariable int roomId,
+                                             @RequestParam(required= false, defaultValue = "false") Boolean rewardMember,
+                                             @RequestParam(required= false, defaultValue = "false") Boolean doubleBonusDay)
+    {
+
+        service.getViewModelInfo(roomId,rewardMember,doubleBonusDay);
+
+
+        return null;
+    }
+
+
+}
---
+++ hotel-booking-service/src/test/java/com/company/trilogyed/hotelbookingservice/controller/HotelControllerTest.java
@@ -0,0 +1,12 @@
+package com.company.trilogyed.hotelbookingservice.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HotelControllerTest {
+
+    @Test
+    public void createRoomViewModel() {
+    }
+}
